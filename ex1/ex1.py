c33d34dd37f5632eff04f772665e371a85e16f8c
X-21 <edison-404@outlook.com>
2018-07-25 22:09:21 +0800

    At home. Add a function for pause, delete a useless txt file.

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -1,11 +1,25 @@
 import numpy as np
 
 
+def pause_func():
+    while input() != '':
+        pass
+
+
 def load_data(filename):
     data = np.loadtxt(filename, delimiter=",")
     return data
 
 
+def warm_up_exercise():
+    print(np.eye(5))
+
+
 if __name__ == '__main__':
+    print('Running warmUpExercise ... \n')
+    print('5x5 Identity Matrix: \n')
+    warm_up_exercise()
+    print('Program paused. Press enter to continue.\n')
+    pause_func()
     data1 = load_data("ex1data1.txt")
     print(data1)
